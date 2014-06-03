2f5424df2919f809871226cf4c3adadf9b39026b
Ruslan Bredikhin <rus.brn@gmail.com>
2014-06-03 11:24:26 -0400

    Message structure changed.

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
+// Listen for messages
 process.on('message', function(message) {
-  switch (message.message) {
+  switch (message.event) {
+    // Start the emulation
     case 'connect':
       setInterval(function() {
         console.log(JSON.stringify({sortOf: 'anObject', initializedWith: message}));
       }, 1000);
       process.send('online');
       break;
+
+    // Shutdown request
     case 'shutdown':
       process.exit(0);
       break; 
+
     default:
       break;
   }
